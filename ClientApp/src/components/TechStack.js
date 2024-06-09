ec4cf081142f3c3a2ae79e2a72e6d460726e4d14
devenshah2018 <46404643+devenshah2018@users.noreply.github.com>
2024-06-09 13:53:17 -0700

    ESLint resolutions.

--- ClientApp/src/components/TechStack.js
+++ ClientApp/src/components/TechStack.js
@@ -13,23 +13,23 @@ export class TechStack extends Component
                     <h5>Platforms/Frameworks</h5>
                     <div class="tech-subsection">
                         <ul class="tech-stack-list">
-                            <li><img src={require('../img/salesforce.png')} /></li>
-                            <li><img src={require('../img/aws.png')} /></li>
-                            <li><img src={require('../img/angularjs.png')} /></li>
-                            <li><img src={require('../img/react.png')} /></li>
-                            <li><img src={require('../img/net.png')} /></li>
-                            <li><img src={require('../img/flask.png')} /></li>
+                            <li><img src={require('../img/salesforce.png')} alt="Salesforce icon"/></li>
+                            <li><img src={require('../img/aws.png')} alt="AWS icon" /></li>
+                            <li><img src={require('../img/angularjs.png')} alt="AngularJS icon" /></li>
+                            <li><img src={require('../img/react.png')} alt="React icon" /></li>
+                            <li><img src={require('../img/net.png')} alt=".NET icon" /></li>
+                            <li><img src={require('../img/flask.png')} alt="Flask icon" /></li>
                         </ul>
                     </div>
                     <h5>Languages/Frameworks</h5>
                     <div class="tech-subsection">
                         <ul class="tech-stack-list">
-                            <li><img src={require('../img/cs.png')} /></li>
-                            <li><img src={require('../img/python.png')} /></li>
-                            <li><img src={require('../img/js.png')} /></li>
-                            <li><img src={require('../img/html.png')} /></li>
-                            <li><img src={require('../img/sql.png')} /></li>
-                            <li><img src={require('../img/ts.png')} /></li>
+                            <li><img src={require('../img/cs.png')} alt="CS icon" /></li>
+                            <li><img src={require('../img/python.png')} alt="Python icon" /></li>
+                            <li><img src={require('../img/js.png')} alt="JavaScript icon" /></li>
+                            <li><img src={require('../img/html.png')} alt="HTMLv icon" /></li>
+                            <li><img src={require('../img/sql.png')} alt="SQL icon" /></li>
+                            <li><img src={require('../img/ts.png')} alt="TypeScript icon" /></li>
                         </ul>
                     </div>
                 </div>
